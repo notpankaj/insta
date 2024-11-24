351abd83ed85dcb85ef8f4148560788a33ea8cd4
pankaj <iampankaj0409@gmail.com>
2024-11-24 22:12:11 +0530

    ...

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
-require("dotenv").config();
-const express = require("express");
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+// import { getReelData, getReelDataFAKE } from "./helper.js";
 
 const app = express();
-app.use(express.json());
-const PORT = process.env.PORT || 5000;
+const port = 8080;
 
-app.get("/", async (req, res) => {
-    try {
-      res.send("hey pankaj")
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("yes");
+});
+app.get("/ping", (req, res) => {
+  return res.send("PONG!");
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+// app.get("/api/scrap/ig", async (req, res) => {
+//   const { url, test } = req.query;
+
+//   if (!url) {
+//     return res.status(400).json({ message: "Url is Missing!" });
+//   }
+//   // TEST
+//   if (test === "TRUE") {
+//     const data = getReelDataFAKE();
+//     return res.json(data);
+//   }
+
+//   // GET REEL DATA
+//   const data = await getReelData(url);
+//   // INCREMENT THE TOTAL DOWNLOAD COUNT
+//   return res.json({
+//     message: "scraping completed successfully",
+//     data: { ...data, test: false },
+//   });
+// });
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
 });
+
+// ruhend-scraper
